3065ca4ca4972ca5715190dda1c8526804eaf7fd
Adarsh P Menon <adarsh1021@gmail.com>
2017-06-04 23:27:51 +0530

    Multiple os support by determining OS

--- clix/clix.py
+++ clix/clix.py
@@ -1,38 +1,77 @@
 import json
-import xerox, pythoncom
-from pyHook import HookManager
-from .gui import clipboard
+import xerox
+from gui import clipboard
+from sys import platform
+if platform == 'linux' or platform == 'linux2':
+	from .pyxhook import HookManager
 
-# clipboard
-clips = []
-# number of active clix GUIs 
-active = 0
-# previously logged key
-prev_Key = None
-
-def OnKeyPress(event):
-	global prev_Key, active
-	if event.Key == 'Space' and prev_Key == 'Lcontrol' and active == 0:
-		active = 1
-		clipboard(clips)
-		active = 0
-		prev_Key = None
-	elif event.Key == 'C' and prev_Key == 'Lcontorl':
-		text = xerox.paste(xsel = True)
-		clips.append(text)
-		print("You just copied: {}".format(text))
-	else:
-		prev_Key = event.Key
-		print(prev_Key)
-
-def main():
-	new_hook = HookManager()
-	new_hook.KeyDown = OnKeyPress
-	new_hook.HookKeyboard()
-	pythoncom.PumpMessages()
+	# clipboard
+	clips = []
+	# number of active clix GUIs
+	active = 0
+	# previously logged key
+	prev_Key = None
 
 
-if __name__ == "__main__":
-	main()
+	def OnKeyPress(event):
+		global prev_Key, active
+		if event.Key == 'space' and prev_Key == 'Control_L' and active == 0:
+			active = 1
+			clipboard(clips)
+			active = 0
+			prev_Key = None
+		elif event.Key == 'c' and prev_Key == 'Control_L':
+			text = xerox.paste(xsel=True)
+			clips.append(text)
+			print("You just copied: {}".format(text))
+		else:
+			prev_Key = event.Key
+
+
+	def main():
+		new_hook = HookManager()
+		new_hook.KeyDown = OnKeyPress
+		new_hook.HookKeyboard()
+		new_hook.start()
+
+
+	if __name__ == "__main__":
+		main()
+
+elif platform=='win32':
+	import pythoncom
+	from pyHook import HookManager
+
+	# clipboard
+	clips = []
+	# number of active clix GUIs
+	active = 0
+	# previously logged key
+	prev_Key = None
+
+	def OnKeyPress(event):
+		global prev_Key, active
+		if event.Key == 'Space' and prev_Key == 'Lcontrol' and active == 0:
+			active = 1
+			clipboard(clips)
+			active = 0
+			prev_Key = None
+		elif event.Key == 'C' and prev_Key == 'Lcontorl':
+			text = xerox.paste(xsel = True)
+			clips.append(text)
+			print("You just copied: {}".format(text))
+		else:
+			prev_Key = event.Key
+
+	def main():
+		new_hook = HookManager()
+		new_hook.KeyDown = OnKeyPress
+		new_hook.HookKeyboard()
+		pythoncom.PumpMessages()
+
+
+	if __name__ == "__main__":
+		main()
+
 	
 
